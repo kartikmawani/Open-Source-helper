2b83d1f168746817e23a77546609e618d43143db
kartikmanwani <kartikmanwani2006@gmail.com>
2026-04-02 19:54:13 +0530

    Authentication

---
+++ frontend/hooks/useAuth.tsx
@@ -0,0 +1,21 @@
+
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useAuth = () => {
+  return useQuery({
+    queryKey: ['auth-user'],
+    queryFn: async () => {
+      try {
+        const res = await axios.get('http://localhost:4000/current_user', {
+          withCredentials: true,  //it is required otherwise backend  will not   recognized required cookies
+        });
+        return res.data;
+      } catch (err) {
+        return null; 
+      }
+    },
+    retry: false,  
+    staleTime: 1000 * 60 * 5,  
+  });
+};
