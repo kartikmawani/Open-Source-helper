ebf879f49c3597efe8c12ddfec39650700573d9f
kartikmanwani <kartikmanwani2006@gmail.com>
2026-05-03 11:53:26 +0530

    frontend auth changes

--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -1,17 +1,17 @@
-
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-
+import axios from "axios";
+import {useQuery} from '@tanstack/react-query'
 export const useAuth = () => {
   return useQuery({
     queryKey: ['auth-user'],
     queryFn: async () => {
       try {
-        const res = await axios.get('http://localhost/api/current_user', {
-          withCredentials: true,  //it is required otherwise backend  will not   recognized required cookies
+         
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/current_user`, {
+          withCredentials: true, 
         });
         return res.data;
       } catch (err) {
+      
         return null; 
       }
     },
